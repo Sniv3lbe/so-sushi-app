53b6b7c776fefea0c0ba5a677b30ce909ade52fc
Sniv3lbe <jimmy.verbist@gmail.com>
2025-03-25 23:26:52 +0100

    hasmany

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 // models/index.js
+
 const Magasin = require('./Magasin');
 const Produit = require('./Produit');
 const Livraison = require('./Livraison');
@@ -7,19 +8,26 @@ const Recuperation = require('./Recuperation');
 const RecuperationDetail = require('./RecuperationDetail');
 const Invoice = require('./Invoice');
 
-// Associations pour Livraisons
+// --- Associations pour Livraisons ---
 Livraison.belongsTo(Magasin, { foreignKey: 'magasinId' });
+// Important : relation "inverse" pour inclure LivraisonDetail via Livraison
+Livraison.hasMany(LivraisonDetail, { foreignKey: 'livraisonId' });
+
 LivraisonDetail.belongsTo(Livraison, { foreignKey: 'livraisonId' });
 LivraisonDetail.belongsTo(Produit, { foreignKey: 'produitId' });
 
-// Associations pour Récupérations
+// --- Associations pour Récupérations ---
 Recuperation.belongsTo(Magasin, { foreignKey: 'magasinId' });
+// Idem : relation "inverse" pour inclure RecuperationDetail via Recuperation
+Recuperation.hasMany(RecuperationDetail, { foreignKey: 'recuperationId' });
+
 RecuperationDetail.belongsTo(Recuperation, { foreignKey: 'recuperationId' });
 RecuperationDetail.belongsTo(Produit, { foreignKey: 'produitId' });
 
-// Association Facture -> Magasin
+// --- Association Facture -> Magasin ---
 Invoice.belongsTo(Magasin, { foreignKey: 'magasinId' });
 
+// On exporte l'ensemble des modèles pour qu'ils puissent être importés
 module.exports = {
   Magasin,
   Produit,
