ceeca3704f3bad3d8c6f752a40492c69985fa34e
Sniv3lbe <jimmy.verbist@gmail.com>
2025-03-25 22:47:40 +0100

    add invoice.js

--- models/index.js
+++ models/index.js
@@ -5,15 +5,22 @@ const Livraison = require('./Livraison');
 const LivraisonDetail = require('./LivraisonDetail');
 const Recuperation = require('./Recuperation');
 const RecuperationDetail = require('./RecuperationDetail');
+const Invoice = require('./Invoice');
 
-// Associations
+// Associations Livraisons
 Livraison.belongsTo(Magasin, { foreignKey: 'magasinId' });
 LivraisonDetail.belongsTo(Livraison, { foreignKey: 'livraisonId' });
 LivraisonDetail.belongsTo(Produit, { foreignKey: 'produitId' });
 
+// Associations Récupérations
 Recuperation.belongsTo(Magasin, { foreignKey: 'magasinId' });
 RecuperationDetail.belongsTo(Recuperation, { foreignKey: 'recuperationId' });
 RecuperationDetail.belongsTo(Produit, { foreignKey: 'produitId' });
+
+// Association Factures
+Invoice.belongsTo(Magasin, { foreignKey: 'magasinId' });
+// => un Invoice aura invoice.magasinId
+//    permet de savoir pour quel magasin la facture est émise
 
 module.exports = {
   Magasin,
@@ -21,6 +28,6 @@ module.exports = {
   Livraison,
   LivraisonDetail,
   Recuperation,
-  RecuperationDetail
+  RecuperationDetail,
+  Invoice
 };
-
